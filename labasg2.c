fc246e05f73e1c163d1e36ea46e93f8950a57646
zaiehilmi <zaiehilmi@gmail.om>
2021-05-05 10:22:26 +0800

    tak berjaya gather lagi tapi scatter dah boleh

--- labasg2.c
+++ labasg2.c
@@ -1,61 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "mpi.h"
 
-#define KIRA 100000
+#define SAIZ 100000
 
 int main (int argc, char *argv[]) {
-    int saiz, rank;
-    int bilhantar, bilterima, punca;
-    float kecil[1], besar;
+    int tugas, rank, bilangan, punca;
+    float kecil[1];
     
-    MPI_Init(&argc, &argv);
+    MPI_Init(&argc,&argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &saiz);
+    MPI_Comm_size(MPI_COMM_WORLD, &tugas);
 
-    float itemhantar[saiz][KIRA], itemterima[KIRA];
-
-
-    for (int i = 0; i < saiz; i++) {
-        for (int j = 0; j < KIRA; j++) {
-            itemhantar[i][j] = rand();
+    float timbalterima[SAIZ], timbalhantar[tugas][SAIZ], minglobal[tugas];
+    srand(time(0));
+    for (int i = 0; i < tugas; i++){
+        for (int j = 0; j < SAIZ; j++){
+            timbalhantar[i][j] = rand();
         }
-    } 
-    // int o = itemhantar[0][0];
-    // printf("rank[%d] hantar: %d\n", rank, o);
-    punca = 0;
-    bilhantar = KIRA;
-    bilterima = KIRA;
-    MPI_Scatter(itemhantar, bilhantar, MPI_FLOAT, 
-                itemterima, bilterima, MPI_FLOAT, 
+    }
+    // printf("%f\n", timbalhantar[1][2]);
+    punca = 1;
+    bilangan = SAIZ;
+
+    MPI_Scatter(timbalhantar, bilangan, MPI_FLOAT,
+                timbalterima, bilangan, MPI_FLOAT,
+                punca, MPI_COMM_WORLD);
+    // printf("[rank %d] menerima %.1f\n", rank, timbalterima[2]);
+
+    kecil[0] = timbalterima[0];
+    int kira = sizeof(timbalterima) / sizeof(timbalterima[0]);
+    for (int i = 0; i < kira; i++){
+        if (timbalterima[i] < kecil[0])
+            kecil[0] = timbalterima[i];
+    }
+    printf("[rank %d] nilai terkecil %.1f\n", rank, kecil[0]);
+    MPI_Gather(kecil, 1, MPI_FLOAT, 
+                minglobal, tugas, MPI_FLOAT, 
                 punca, MPI_COMM_WORLD);
     
-    // printf("rank [%d]\n", rank);
-    kecil[0] = itemterima[0];
-
-    int _size = sizeof(itemterima)/sizeof(itemterima[0]);
-
-    for (int k = 0; k < _size; k++) {
-        if (itemterima[k] < kecil[0]) {
-            itemterima[0] = itemterima[k];
-        }
-    }
-    printf("rank[%d] terkecil: %.1f\n", rank, itemterima[0]);
-
-    MPI_Gather(itemterima, bilhantar, MPI_FLOAT, 
-                itemhantar, bilhantar, MPI_FLOAT, 
-                punca, MPI_COMM_WORLD);
-    if (rank == 0){
-        float *_kecil = itemhantar[0];
-        for (int k = 0; k < _size; k++) {
-            printf("index %d: %f", k, *itemhantar[k]);
-            // if (itemhantar[0] < _kecil) {
-            //     _kecil = itemhantar[0];
-            // }
-        }
-        printf("terkecil dalam dunia ini ialah ==== %.1f\n", *_kecil);
-    }
+    if (rank == 1) {
+        kecil[0] = minglobal[0];
+        int kiraglobal = sizeof(minglobal) / sizeof(minglobal[0]);
+        for (int i = 0; i < tugas; i++){
+            if (minglobal[i] < kecil[0])
+                kecil[0] = minglobal[i];
         
+            printf("[rank %d] nilai global terkecil %.1f\n", rank, kecil[0]);
+        }
+
+    }
 
     MPI_Finalize();
 }
