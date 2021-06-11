42925f2cd1bb095cf7c35673158d74c6539d7df1
zaiehilmi <zaiehilmi@gmail.om>
2021-06-11 19:47:01 +0800

    betulkan sikit

--- tugasan/3_pi-calc.py
+++ tugasan/3_pi-calc.py
@@ -1,11 +1,11 @@
-#shell syntax: mpiexec -np BIL_PROSESOR -quiet python3 NAMA_FAIL
-#contoh: mpiexec -np 4 -quiet python3 3_pi-calc.py
+# shell syntax: mpiexec -np BIL_PROSESOR -quiet python3 NAMA_FAIL
+# contoh: mpiexec -np 4 -quiet python3 3_pi-calc.py
 
 from random import random, seed
 from mpi4py import MPI
 from time import time
 
-masa = time()
+masa = MPI.Wtime()
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
@@ -14,22 +14,22 @@ size = comm.Get_size()
 circle_count = 0
 num = 10000/size
 
-seed(rank)
+seed(rank * time())
 for i in range(int(num)):
     x = random()
     y = random()
     if ((x * x) + (y * y)) < 1:
         circle_count += 1
 
-mypi = (4.0 * circle_count) / num
-if rank == 0:
-   print(f"rank[{rank}] mypi = {mypi}")
-   
-kira = comm.reduce(mypi, op=MPI.SUM, root=0)
+localPi = (4.0 * circle_count) / num
+
+if (rank == 0):
+    print(f"rank[{rank}] pi = {localPi}")
+    
+kira = comm.reduce(localPi, op=MPI.SUM, root=0)
 
 if rank == 0:
-    pi = float((1.0 / size) * kira)
+    pi = float(kira / size)
     
-    print(f"Computed value of pi on {size} processors is {pi}")
-    print(f"Using {int(size * num)} samples.")
-    print(f'====================\nExecution time {round((time() - masa), 4)} seconds')
+    print(f"Computed value of pi on {size} processors is {round(pi, 4)}")
+    print(f'====================\nExecution time {round((MPI.Wtime() - masa), 4)} seconds')
